bc927a6a786909546a6299c9d644bc5e56bb5962
claranobre <aclaranobre@gmail.com>
2015-09-30 22:57:40 -0300

    Adicionando o main

---
+++ main.cpp
@@ -0,0 +1,21 @@
+class MyKeyComparator{
+	public:
+		int operator()(int lhs, int rhs) const {
+		if(lhs < rhs)	return -1;
+		}
+		else if (lhs == rhs)
+			return 0;
+		else
+			return 1; //lhs>rhs
+};
+
+int main(){
+	DictionarySAL <int, std::string, MyKeyComparator> myList (50);
+
+	cout << ">>> Inserindo {2015003129, "\Jack\"}" << endl;
+	myList.insert(2015003129, "Jack");
+
+	cout << ">>> Inserindo {2014065190, "\John\"}" << endl;
+	myList.insert(2014065190, "John");
+
+}
